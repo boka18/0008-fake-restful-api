ed58426c69b6d73affbc1e2fafe922b4ed84669a
boka18 <bokasmoka21@gmail.com>
2023-12-22 16:24:09 +0100

    updates index to support /auth route

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const { exec } = require('child_process');
+const jsonServer = require('json-server');
+const bodyParser = require('body-parser');
 
-// Set port (default: 3000). For Heroku, we need to use
-// the port set by the environment variable $PORT
+// Create a new JSON Server instance
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // You can provide an empty object if you don't want to use db.json
+const middlewares = jsonServer.defaults();
+
+// Use default middlewares (CORS, static, etc.)
+server.use(middlewares);
+server.use(bodyParser.json());
+
+// Add custom authentication logic here
+server.post('/auth', (req, res) => {
+  // Your authentication logic goes here
+  // Example: Check username and password from request body
+  console.log(req.body)
+  // const { username, password } = req.body;
+
+  // Your authentication logic...
+
+  if (true) {
+    res.jsonp({
+      access_token: 'your_generated_token',
+      token_type: 'Bearer',
+    });
+  } else {
+    res.status(401).jsonp({ error: 'Unauthorized' });
+  }
+});
+
+// Use the JSON Server router
+server.use(router);
+
+// Set port (default: 3000). For Heroku, use the port set by the environment variable $PORT
 const port = process.env.PORT || 3000;
 
-const command = `json-server --watch db.json --port ${port}`;
-
-exec(command, (err, stdout, stderr) => {
-  if (err) {
-    console.log('Error running exec', err);
-    return;
-  }
-  console.log('stdout:', stdout);
-  console.log('stderr:', stderr);
+// Start the JSON Server
+server.listen(port, () => {
+  console.log(`JSON Server is running on http://localhost:${port}`);
 });
